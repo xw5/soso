3459a731134606bf3ca62a53e1fa043df5a6d2c7
谢武 <xiewu1@meizu.com>
2023-05-06 22:31:03 +0800

    实现content-script

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-center">
+    <div>soso主页面</div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainPage'
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,7 @@
 const routes = [
   { path: '/options', component: () => import('pages/OptionsPage.vue') },
   { path: '/popup', component: () => import('pages/PopupPage.vue') },
+  { path: '/main', component: () => import('pages/MainPage.vue') },
 ]
 
 export default routes
